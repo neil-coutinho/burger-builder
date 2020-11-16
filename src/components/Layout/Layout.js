aa35772a52267a297070baf85ed956105b2eca1c
Neil Coutinho <njcoutinho@gmail.com>
2020-11-16 15:48:23 +0530

    feature: BurgerBuilder component setup

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Aux from "../../Aux";
+import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
+import styles from "./Layout.module.css";
+
 const layout = (props) => {
 
     return (
         // <div className="Layout"> => wrapping el to allow ajacent JSX
         <Aux>
-            <div>Toolbar, Sidebar</div>
-            <main>{props.children}</main>
+            <div className={styles.toolbar}>Toolbar, Sidebar</div>
+            <main className={styles.main}>
+                <BurgerBuilder/>
+            </main>
         </Aux>
             
         // </div>
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import react, { Component } from "react"; 
+import Aux from "../../Aux";
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger Preview</div>
+                <div>Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
