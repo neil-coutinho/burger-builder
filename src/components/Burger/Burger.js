c2243c3fcdf4de9f6ae9fab8fec62a55c1fe1a39
Neil Coutinho <njcoutinho@gmail.com>
2020-11-17 16:57:45 +0530

    feature: iterate over props.ingredients in burger component, feature: default ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,23 @@ import React from "react";
 import Ingredient from "./Ingredient/Ingredient";
 import styles from "./Burger.module.css";
 
-const burger = () => {
+const burger = (props) => {
+    
+    let ingredients = [];
+
+    for(const [ingredient, count] of props.ingredients) {
+      
+        const layers =   [...Array(count)]
+                                .map((_,index) => { return <Ingredient type={ingredient} key={ `${ingredient}_${index}` }></Ingredient>})
+       
+
+        ingredients = [...ingredients, ...layers]
+    }
+
     return (
         <div className={styles.Burger}>
             <Ingredient type="bread-top"></Ingredient>
-            <Ingredient type="cheese"></Ingredient>
-            <Ingredient type="bacon"></Ingredient>
-            <Ingredient type="meat"></Ingredient>
+            {ingredients}
             <Ingredient type="bread-bottom"></Ingredient>
         </div>
         
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,27 @@ import react, { Component } from "react";
 import Aux from "../../Aux";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
+
+    constructor() {
+        super()
+        const burger = new Map();
+        burger.set('salad', 1);
+        burger.set('bacon', 2);
+        burger.set('meat', 2);
+
+        this.state = {
+            ingredients : burger
+        }
+
+
+    }
+
+
     render() {
+     
         return (
             <Aux>
-                <div><Burger/></div>
+                <div><Burger ingredients={this.state.ingredients}/></div>
                 <div>Controls</div>
             </Aux>
         );
