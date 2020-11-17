a2c83ccc7ab836f9052289322a20337f831ab46e
Neil Coutinho <njcoutinho@gmail.com>
2020-11-17 09:14:16 +0530

    feature switch case for ingredient based on type prop

--- src/components/Burger/Ingredient/Ingredient.js
+++ src/components/Burger/Ingredient/Ingredient.js
@@ -10,6 +10,31 @@ const ingredient = (props) => {
                             <div className={styles.Seeds2}></div>
                         </div>
             break;
+        case "bread-bottom":
+            content =   <div className={styles.BreadBottom}>
+                    
+                        </div>
+            break;
+        case "meat":
+            content =   <div className={styles.Meat}>
+                    
+                        </div>
+            break;
+        case "cheese":
+            content =   <div className={styles.Cheese}>
+                    
+                        </div>
+            break;
+        case "salad":
+            content =   <div className={styles.Salad}>
+                    
+                        </div>
+            break;
+        case "bacon":
+            content =   <div className={styles.Bacon}>
+                    
+                        </div>
+            break;
         default:
             content = null;
     }
