376ffc520443ee460d82969eeb12556de113c0b1
Neil Coutinho <njcoutinho@gmail.com>
2020-12-24 09:48:51 +0530

    Display error notification modal on axios interceptor error

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Modal from "../Modal/Modal";
+import Aux from "../../Aux";
 
 
 const errorComponent = (WrappedComponent, axios) => {
@@ -6,26 +8,40 @@ const errorComponent = (WrappedComponent, axios) => {
 
     return class extends Component {
 
+        state = {
+            showModal: false,
+        }
+
         componentDidMount() {
            
             axios.interceptors.request.use((request) => {
                 console.log({request});
+                this.setState({showModal: false});
                 return request;
             });
 
 
-            axios.interceptors.response.use((response) => {
+            axios.interceptors.response.use(null,(response) => {
                 console.log({response});
+                this.setState({showModal: true});
                
             });
 
 
         }
 
+        dismiss() {
+            this.setState({showModal: false});
+        }
 
         render() {
-            return <WrappedComponent/>
-        }
+            return (
+            <Aux>
+                <Modal show={this.state.showModal} dismiss={this.dismiss.bind(this)}>
+                    Something went wrong.
+                </Modal>
+                <WrappedComponent {...this.props}/>
+            </Aux>)}
     }
 
   
