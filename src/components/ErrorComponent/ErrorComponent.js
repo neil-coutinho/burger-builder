283b9edef1a78be94eb2e5000a83a2626eb3e375
Neil Coutinho <njcoutinho@gmail.com>
2020-12-24 09:08:40 +0530

    Created axios request and response interceptors

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 
 
 const errorComponent = (WrappedComponent, axios) => {
-    console.log({WrappedComponent, axios});
+   
 
     return class extends Component {
+
+        componentDidMount() {
+           
+            axios.interceptors.request.use((request) => {
+                console.log({request});
+                return request;
+            });
+
+
+            axios.interceptors.response.use((response) => {
+                console.log({response});
+               
+            });
+
+
+        }
+
+
         render() {
             return <WrappedComponent/>
         }
